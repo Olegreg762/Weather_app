09e111f09e76bddce5a8123aaba2a58caae9c8fc
Olegreg762 <109924953+Olegreg762@users.noreply.github.com>
2023-07-12 18:33:13 -0500

    update script set local storeage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,42 @@
-let date = dayjs()
+$(function () {
+let date = dayjs();
+let i = 0;
+let event = "";
 
-$("#city").text("Topeka")
-$("#date").text(date.format("MMM/DD/YYYY"))
+
+$("#search_btn").on("click", function(){
+    // Records the text content of the div for the button press
+    event = $(this).siblings("#city_input").val();
+    $("#city").text(event)
+    // Creates and displays the popup message element when an event is added to the local storage
+    if(i == 0){
+        $(`#history_${i}`).text(`${event}`)
+        localStorage.setItem(`#history_${i}`,event);
+        $(`#history_${i}`).removeClass("invisible").addClass("visible");
+        i++
+    }else{
+        $(`#history_${i}`).text(`${event}`)
+        localStorage.setItem(`#history_${i}`,event);
+        $(`#history_${i}`).removeClass("invisible").addClass("visible");
+        i++
+        if(i>4){
+            i = 0
+        }
+    }
+    
+  });
+
+  function show_local(){
+    const num_elements = 5
+    for(let i = 0; i < num_elements; i++){
+        $(`#history_${i}`).text(`${event}`)
+        $(`.history${i}`).text(localStorage.getItem(`#history_${i}`))
+        if(localStorage.getItem(`#history_${i}`) != undefined){
+            $(`#history_${i}`).removeClass("invisible").addClass("visible");
+            }
+        }
+    };
+
+    show_local(); 
+$("#date").text(date.format("MMM/DD/YYYY"));
+});
