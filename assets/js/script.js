73a92c8d575a7cdecf52a378b042a3d02ccfaf71
Olegreg762 <109924953+Olegreg762@users.noreply.github.com>
2023-07-12 21:47:30 -0500

    Update Index and script
    Index add history_button class to buttons under <ul id="history"> this will allow script to pick up button presses

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,39 +4,44 @@ let i = 0;
 let event = "";
 
 
-$("#search_btn").on("click", function(){
-    // Records the text content of the div for the button press
-    event = $(this).siblings("#city_input").val();
-    $("#city").text(event)
-    // Creates and displays the popup message element when an event is added to the local storage
-    if(i == 0){
-        $(`#history_${i}`).text(`${event}`)
-        localStorage.setItem(`#history_${i}`,event);
-        $(`#history_${i}`).removeClass("invisible").addClass("visible");
-        i++
-    }else{
-        $(`#history_${i}`).text(`${event}`)
-        localStorage.setItem(`#history_${i}`,event);
-        $(`#history_${i}`).removeClass("invisible").addClass("visible");
-        i++
-        if(i>4){
-            i = 0
-        }
-    }
-    
-  });
-
-  function show_local(){
-    const num_elements = 5
-    for(let i = 0; i < num_elements; i++){
-        $(`#history_${i}`).text(localStorage.getItem(`#history_${i}`))
-        if(localStorage.getItem(`#history_${i}`) != null){
+    $("#search_btn").on("click", function(){
+        // Records the text content of the div for the button press
+        event = $(this).siblings("#city_input").val();
+        $("#city").text(event)
+        // Creates and displays the popup message element when an event is added to the local storage
+        if(i == 0){
+            $(`#history_${i}`).text(`${event}`)
+            localStorage.setItem(`#history_${i}`,event);
             $(`#history_${i}`).removeClass("invisible").addClass("visible");
+            i++
+        }else{
+            $(`#history_${i}`).text(`${event}`)
+            localStorage.setItem(`#history_${i}`,event);
+            $(`#history_${i}`).removeClass("invisible").addClass("visible");
+            i++
+            if(i>4){
+                i = 0
             }
-        console.log(localStorage.getItem(`#history_${i}`))
         }
-    };
+        
+    });
+
+    function show_local(){
+        const num_elements = 5
+        for(let i = 0; i < num_elements; i++){
+            $(`#history_${i}`).text(localStorage.getItem(`#history_${i}`))
+            if(localStorage.getItem(`#history_${i}`) != null){
+                $(`#history_${i}`).removeClass("invisible").addClass("visible");
+                }
+            }
+        };
+
+    $("#history").on("click", ".history_button", function(){
+        let button_id = $(this).attr("id");
+        let button_storage = localStorage.getItem(`#${button_id}`);
+        $("#city").text(button_storage)
+    });
 
     show_local(); 
-$("#date").text(date.format("MMM/DD/YYYY"));
+    $("#date").text(date.format("MMM/DD/YYYY"));
 });
