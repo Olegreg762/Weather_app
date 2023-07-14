e6743b0a62062660bcbd9f5212e9816f84d0b9d5
Olegreg762 <109924953+Olegreg762@users.noreply.github.com>
2023-07-14 11:12:34 -0500

    script update for api GET weather data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,23 @@
 $(function () {
+
 let date = dayjs();
 let i = 0;
 let event = "";
+let city_name = "";
+let state_code = "";
+let country_code = ""
+let exclude = "minutely, hourly, alerts"
 
+const api_key = window.config.api_key;
+
+    $("#date").text(date.format("MMM/DD/YYYY"));
 
     $("#search_btn").on("click", function(){
         // Records the text content of the div for the button press
         event = $(this).siblings("#city_input").val();
+        city_name = event
+        get_lon_lat()
         $("#city").text(event)
-        // Creates and displays the popup message element when an event is added to the local storage
         if(i == 0){
             $(`#history_${i}`).text(`${event}`)
             localStorage.setItem(`#history_${i}`,event);
@@ -40,8 +49,35 @@ let event = "";
         let button_id = $(this).attr("id");
         let button_storage = localStorage.getItem(`#${button_id}`);
         $("#city").text(button_storage)
+        city_name = button_storage
+        get_lon_lat()
     });
 
     show_local(); 
-    $("#date").text(date.format("MMM/DD/YYYY"));
+
+
+    function get_lon_lat(){
+        const weather_geo_url = `http://api.openweathermap.org/geo/1.0/direct?q=${city_name},${state_code},${country_code}&limit=1&appid=${api_key}`;
+        fetch(weather_geo_url)
+            .then(function(response){
+                return response.json();
+            })
+            .then(function(data){
+                console.log(data);
+                let lat = data[0].lat;
+                let lon = data[0].lon;
+                return lat, lon
+            })
+            .then(function(lat, lon){
+                const weather_data_url = `https://api.openweathermap.org/data//2.5/forecast?lat=${lat}&lon=${lon}&exclude=${exclude}&appid=${api_key}`;
+                fetch(weather_data_url)
+                    .then(function(response){
+                        console.log(lat + lon)
+                        return response.json();
+                    })
+                    .then(function(data){
+                        console.log(data)
+                    })
+            })
+    };
 });
